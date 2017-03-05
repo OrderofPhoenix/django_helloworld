048d3835d07f8d2a1c53b7f4c2ccaa4c1bf28823
amazing-tiantian <MacbookAir@MacBook-Air.local>
2017-03-05 17:56:01 +0800

    first commit

---
+++ bbs/urls.py
@@ -0,0 +1,15 @@
+from . import views
+from django.conf.urls import url
+
+app_name = 'bbs'
+urlpatterns = [
+    # ex: /polls/
+    url(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/like/$', views.like, name='like'),
+
+]
